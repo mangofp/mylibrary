12fef1a5615cad2e9c7ba2425210a3156a5798f2
Andres Järviste <andres.jarviste@wbscodingschool.com>
2021-10-07 21:44:20 +0300

    insert

--- controllers.js
+++ controllers.js
@@ -1,6 +1,34 @@
 
+const pgp = require('pg-promise')()
+
+const username = process.env.DB_USER
+const password = process.env.DB_PASS
+const host = process.env.DB_HOST
+const port = process.env.DB_PORT
+
+const uri = `postgres://${username}:${password}@${host}:${port}/library2`
+
+console.log(uri)
+const db = pgp(uri)
+
 async function addBook(book) {
-    return book;
+    const newBook = {
+        title:  book.title,
+        description: book.description,
+        author_id: book.authorId 
+    }
+
+    const result = await db.query('INSERT INTO books(${this:name}) VALUES(${this:csv})', newBook)
+    return newBook;
+}
+
+async function getBooks() {
+    const books = await db.query('SELECT ${columns:name} FROM ${table:name}', {
+        columns: ['column1', 'column2'],
+        table: 'table'
+    });
+
+    return books
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const { addBook } = require('./controllers')
+require('dotenv').config()
 const cors = require('cors')
+
+const { addBook, getBooks } = require('./controllers')
 
 const app = express()
 app.use(express.json())
@@ -11,6 +13,11 @@ const PORT = process.env.PORT || 8080
 app.post("/book", async (req, res) => {
     const newBook = req.body
     const result = await addBook(newBook)
+    res.status(201).send(result)
+})
+
+app.get("/book", async (req, res) => {
+    const result = await getBooks()
     res.status(201).send(result)
 })
 
