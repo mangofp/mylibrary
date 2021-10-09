75e1fdb161567e181ceee8ce0277b6867c0cdd61
Andres Järviste <andres.jarviste@wbscodingschool.com>
2021-10-09 12:13:18 +0300

    ..

--- controllers.js
+++ controllers.js
@@ -1,33 +1,21 @@
 
 const pgp = require('pg-promise')()
-
-const username = process.env.DB_USER
-const password = process.env.DB_PASS
-const host = process.env.DB_HOST
-const port = process.env.DB_PORT
-
-let uri = `postgres://${username}:${password}@${host}:${port}/library2`
 let db
+
 if (process.env.DATABASE_URL) {
     db = pgp({
         connectionString: process.env.DATABASE_URL,
         ssl: { rejectUnauthorized: false }
     })
 } else {
+    const username = process.env.DB_USER
+    const password = process.env.DB_PASS
+    const host = process.env.DB_HOST
+    const port = process.env.DB_PORT
+
+    let uri = `postgres://${username}:${password}@${host}:${port}/library2`
     db = pgp(uri)
 }
-
-//const cn = {
-//    database: "d3etg4uerniqdl",
-//    host: "ec2-34-199-15-136.compute-1.amazonaws.com",
-//    port: "5432",
-//    user: "bvlhcayvqnzhcf",
-//    password: "81d4c1f7a3013e4a31b101a0b245c3caa812093b4fc3cb960fbef07d282987bb",
-//    ssl: { rejectUnauthorized: false },
-//    sslmode: "require"
-//}
-
-//const db = pgp(cn);
 
 async function addBook(book) {
     const newBook = {
@@ -56,7 +44,6 @@ async function getOneBook(id) {
         bookid: id
 
     });
-
 }
 
 module.exports = {
